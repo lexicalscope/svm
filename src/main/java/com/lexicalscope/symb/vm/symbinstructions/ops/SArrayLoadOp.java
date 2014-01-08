7b28af49b030c0bb0f34e722018c46ed5152947d
lexicalscope <lexicalscope@gmail.com>
2014-01-08 23:24:52 +0000

    symbolic array load

--- src/main/java/com/lexicalscope/symb/vm/symbinstructions/ops/SArrayLoadOp.java
+++ src/main/java/com/lexicalscope/symb/vm/symbinstructions/ops/SArrayLoadOp.java
@@ -8,6 +8,10 @@ import com.lexicalscope.symb.vm.StackFrame;
 import com.lexicalscope.symb.vm.Statics;
 import com.lexicalscope.symb.vm.Vop;
 import com.lexicalscope.symb.vm.concinstructions.ops.ArrayLoadOp;
+import com.lexicalscope.symb.vm.symbinstructions.ops.array.NewSymbArray;
+import com.lexicalscope.symb.vm.symbinstructions.symbols.IArraySelectSymbol;
+import com.lexicalscope.symb.vm.symbinstructions.symbols.IArraySymbol;
+import com.lexicalscope.symb.vm.symbinstructions.symbols.IConstSymbol;
 import com.lexicalscope.symb.vm.symbinstructions.symbols.ISymbol;
 import com.lexicalscope.symb.z3.FeasibilityChecker;
 
@@ -28,14 +32,14 @@ public class SArrayLoadOp implements Vop {
       if(arrayLength instanceof Integer && offset instanceof Integer) {
          stackFrame.push(concreteArrayLoad.loadFromHeap(heap, arrayref, (int) offset));
       } else if (arrayLength instanceof ISymbol) {
-         loadFromSymbolicArray(heap, arrayref, offset);
+         stackFrame.push(loadFromSymbolicArray(heap, arrayref, offset));
       } else {
          throw new UnsupportedOperationException("concrete array symbolic offset");
       }
    }
 
-   private void loadFromSymbolicArray(final Heap heap, final Object arrayref, final Object offset) {
-      throw new UnsupportedOperationException("load from symbolic array");
+   private Object loadFromSymbolicArray(final Heap heap, final Object arrayref, final Object offset) {
+      return new IArraySelectSymbol((IArraySymbol) heap.get(arrayref, NewSymbArray.ARRAY_SYMBOL_OFFSET), offset instanceof ISymbol ? (ISymbol) offset : new IConstSymbol((int) offset));
    }
 
    @Override public String toString() {
