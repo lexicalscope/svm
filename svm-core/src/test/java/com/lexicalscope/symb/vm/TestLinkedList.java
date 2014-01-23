0d0163ea3bad3c8f9dce098e2b6ef6b14f73b2a9
lexicalscope <lexicalscope@gmail.com>
2014-01-23 15:04:17 +0000

    fix build problem

--- svm-core/src/test/java/com/lexicalscope/symb/vm/TestLinkedList.java
+++ svm-core/src/test/java/com/lexicalscope/symb/vm/TestLinkedList.java
@@ -4,12 +4,22 @@ import static com.lexicalscope.symb.vm.StateMatchers.normalTerminiationWithResul
 import static com.lexicalscope.symb.vm.conc.VmFactory.concreteVm;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 import com.lexicalscope.symb.vm.conc.MethodInfo;
 
 public class TestLinkedList {
    private final MethodInfo linkedListAddRemove = new MethodInfo(StaticLinkedList.class, "addRemove", "(I)I");
+
+   public static class StaticLinkedList {
+      public static int addRemove(final int x) {
+         final ArrayList<Integer> list = new ArrayList<>();
+         list.add(x);
+         return list.remove(0);
+      }
+   }
 
    @Test public void linkedListAddThenGet() {
       final Vm<State> vm = concreteVm(linkedListAddRemove, 4);
