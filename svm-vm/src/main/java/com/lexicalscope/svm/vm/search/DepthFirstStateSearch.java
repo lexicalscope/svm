7e577163ea880eb6db42c4768b7e6c5358da0d90
lexicalscope <lexicalscope@gmail.com>
2014-03-03 19:02:46 +0000

    search round robin amongst the starting states

--- svm-vm/src/main/java/com/lexicalscope/svm/vm/search/DepthFirstStateSearch.java
+++ svm-vm/src/main/java/com/lexicalscope/svm/vm/search/DepthFirstStateSearch.java
@@ -1,14 +1,17 @@
 package com.lexicalscope.svm.vm.search;
 
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Deque;
+import java.util.List;
 
 import com.lexicalscope.svm.vm.StateSearch;
 
 public class DepthFirstStateSearch<S> implements StateSearch<S> {
-   private final Deque<S> pending = new ArrayDeque<>();
+   private final List<Deque<S>> pending = new ArrayList<>();
    private final Deque<S> finished = new ArrayDeque<>();
+   private int current = 0;
 
    @Override
    public S firstResult() {
@@ -17,25 +20,40 @@ public class DepthFirstStateSearch<S> implements StateSearch<S> {
 
    @Override
    public void reachedLeaf() {
-      finished.push(pending.pop());
-   }
-
-   @Override
-   public S pendingState() {
-      return pending.peek();
-   }
-
-   @Override
-   public void fork(final S[] states) {
-      pending.pop();
-      for (final S state : states) {
-         pending.push(state);
+      finished.push(pending().pop());
+      if(pending().isEmpty()) {
+         pending.remove(current);
+         if(!pending.isEmpty()) {
+            current = current % pending.size();
+         }
+      } else {
+         roundRobin();
       }
    }
 
    @Override
+   public S pendingState() {
+      return pending().peek();
+   }
+
+   @Override
+   public void fork(final S[] states) {
+      pending().pop();
+      for (final S state : states) {
+         pending().push(state);
+      }
+      roundRobin();
+   }
+
+   private void roundRobin() {
+      current = (current + 1) % pending.size();
+   }
+
+   @Override
    public void consider(final S state) {
-      pending.push(state);
+      final ArrayDeque<S> pendingStack = new ArrayDeque<>();
+      pendingStack.push(state);
+      pending.add(pendingStack);
    }
 
    @Override
@@ -44,6 +62,13 @@ public class DepthFirstStateSearch<S> implements StateSearch<S> {
    }
 
    @Override public void goal() {
-      // nothing
+      roundRobin();
+   }
+
+   private Deque<S> pending() {
+      if(pending.isEmpty()) {
+         return new ArrayDeque<>();
+      }
+      return pending.get(current);
    }
 }
