dcff703e8ead37e15e39e24218d80041d1fe5d35
lexicalscope <lexicalscope@gmail.com>
2014-06-07 13:01:21 +0100

    split out matchers

--- matchers-additional/src/main/java/com/lexicalscope/Transform.java
+++
@@ -1,8 +0,0 @@
-package com.lexicalscope;
-
-import org.hamcrest.Description;
-import org.hamcrest.SelfDescribing;
-
-public interface Transform<T, S> extends SelfDescribing {
-   T transform(S item, Description mismatchDescription);
-}
